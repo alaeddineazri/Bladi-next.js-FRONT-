b68865f00ff4aee0e24fd56cbfa4e12c7fccc730
AlaeddineAzri <alaeddinazri@gmail.com>
2022-04-16 17:38:28 +0100

    404

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import Layout from "../components/Layout";
+import styles from "../styles/404.module.css";
+
+const NotFoundPage = () => {
+  return (
+    <Layout >
+    <div className={styles.notFound}>
+        <h1>404</h1>
+    </div>
+    </Layout>
+  );
+};
+
+export default NotFoundPage;
