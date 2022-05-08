2ede275fbf1bfc0cc12f175d759d1d8d4b61080d
AlaeddineAzri <alaeddinazri@gmail.com>
2022-05-08 22:12:47 +0100

    fix

--- pages/api/events/[slug].js
+++ pages/api/events/[slug].js
@@ -1,8 +1,10 @@
 const {events} = require ("./data.json")
 
 export default function handler(req, res) {
+    const evt = events.find(evt => evt.slug === req.query.slug)
+
   if (req.method === "GET") {
-  res.status(200).json(events)
+  res.status(200).json(evt)
   } else {
     res.setHedaer("Allow", ["GET"])
     res.status(405).json({
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,35 @@
-import Layout from '@/components/Layout'
+import EventItem from "@/components/EventItem";
+import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
+import Link from "next/link";
 
-
-
-export default function Home() {
+export default function Home({ events }) {
+  console.log(events);
   return (
     <Layout>
       <h1>home</h1>
+      {events.length === 0 && <h3>No events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+      {events.length > 0 && (
+        <Link href="/events">
+          <a className="btn-secondary">View All Events</a>
+        </Link>
+      )}
     </Layout>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+  return {
+    props: {
+      events,
+      revalidate: 1,
+    },
+  };
 }
